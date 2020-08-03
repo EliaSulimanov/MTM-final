8514930fe51a879c110d4ea347e9374e7a5a4e6d
Elia Sulimanov <eliasulimanov@gmail.com>
2020-08-03 23:17:42 +0300

    Added fatal error

--- GraphException.h
+++ GraphException.h
@@ -17,6 +17,18 @@ namespace gcalc
 			return message.c_str();
 		}
 	};
+
+	class FatalGraphException : public std::exception
+	{
+	private:
+		std::string message;
+	public:
+		FatalGraphException(std::string message) noexcept : message(message) {}
+		virtual const char* what() const noexcept override
+		{
+			return message.c_str();
+		}
+	};
 }
 
 #endif
