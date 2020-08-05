ca82cc5b58d14ee7fdeec3f3bb0153b7561026e7
Elia Sulimanov <eliasulimanov@gmail.com>
2020-08-05 13:19:43 +0300

    Update gcalc.cpp

--- gcalc.cpp
+++ gcalc.cpp
@@ -15,43 +15,54 @@ void eval(std::map<std::string, std::shared_ptr<gcalc::Graph>>& symbol_map, std:
 void gcalcLoop(std::map<std::string, std::shared_ptr<gcalc::Graph>>& symbol_map) {
 	while (true)
 	{
-		std::string command;
+		try
+		{
+			std::string command;
 
-		std::cout << "Gcalc> ";
-		std::getline(std::cin, command);
-		std::vector<std::string> split_command = gcalc::GraphHelper::splitCommand(command);
-		
-		if (split_command.size() == 0)
-		{
-			continue;
-		}
-		else if (split_command.size() == 1)
-		{
-			if (split_command[0].compare("quit") == 0)
+			std::cout << "Gcalc> ";
+			std::getline(std::cin, command);
+			std::vector<std::string> split_command = gcalc::GraphHelper::splitCommand(command);
+
+			if (split_command.size() == 0)
 			{
-				break;
+				continue;
 			}
-			else if (split_command[0].compare("reset") == 0)
+			else if (split_command.size() == 1)
 			{
-				symbol_map.clear();
-			}
-			else if (split_command[0].compare("who") == 0) 
-			{
-				for (auto grap : symbol_map)
+				if (split_command[0].compare("quit") == 0)
 				{
-					if (grap.second != nullptr)
+					break;
+				}
+				else if (split_command[0].compare("reset") == 0)
+				{
+					symbol_map.clear();
+				}
+				else if (split_command[0].compare("who") == 0)
+				{
+					for (auto grap : symbol_map)
 					{
-						std::cout << grap.first << std::endl;
+						if (grap.second != nullptr)
+						{
+							std::cout << grap.first << std::endl;
+						}
 					}
 				}
+				else
+				{
+					std::cout << "Error: Unrecognized command" << std::endl;
+				}
 			}
-			else
-			{
-				std::cout << "Error: Unrecognized command" << std::endl;
+			else {
+				eval(symbol_map, split_command);
 			}
 		}
-		else {
+		catch (gcalc::FatalGraphException& e)
+		{
 			// TODO
+		}
+		catch (std::exception& e)
+		{
+			std::cout << "Error: " << e.what() << std::endl;
 		}
 	}
 }
@@ -70,65 +81,63 @@ void eval(std::map<std::string, std::shared_ptr<gcalc::Graph>>& symbol_map, std:
 {
 	try
 	{
-		if (gcalc::GraphHelper::checkNoDuplicateCommands(command) && gcalc::GraphHelper::checkSpecialChars(command)) // TODO: see if this is a good check
+		if (gcalc::GraphHelper::checkNoDuplicateCommands(command)) // TODO: see if this is a good check
 		{
-			/*if (command.size() == 4) //easy command, no graph evaluation
-			{
-				if (command[0].compare("print") == 0)
-				{
-					auto graph_name = symbol_map.find(command[2]);
-					if (graph_name != symbol_map.end()) // Variable exist
-					{
-						if (graph_name->second != nullptr)
-						{
-							gcalc::print(*(graph_name->second));
-						}
-						else // Lost a graph
-						{
-							throw gcalc::FatalGraphException("Memory error occurred");
-						}
-					} 
-					else
-					{
-						throw gcalc::GraphException("Undefined variable");
-					}
-				}
-				else if (command[0].compare("delete") == 0)
-				{
-					auto graph_name = symbol_map.find(command[2]);
-					if (graph_name != symbol_map.end()) // Variable exist
-					{
-						if (graph_name->second != nullptr)
-						{
-							deleteGraph(symbol_map, graph_name->first); // TODO: check if memory leak
-						}
-						else // Lost a graph
-						{
-							throw gcalc::FatalGraphException("Memory error occurred");
-						}
-					}
-					else
-					{
-						throw gcalc::GraphException("Undefined variable");
-					}
-				}
-			}
-			else // complex command
-			{
-				// TODO
-			}*/
-
 			if (command[0].compare("print") == 0)
 			{
+				if (command[1].compare("(") != 0)
+				{
+					throw gcalc::GraphException("Invalid syntax");
+				}
+				if (command.size() == 4) // The arg is graph name
+				{
+					if (symbol_map.find(command[2]) != symbol_map.end())
+					{
+						gcalc::print(*(symbol_map.at(command[2])));
+					}
+					else
+					{
+						throw gcalc::GraphException("Undefined variable");
+					}
+				}
+				else // The arg is complex graph def
+				{
+					// TODO
+				}
 				
 			}
-			else if (command[0].compare("delete") == 0)
+			else if (command[0].compare("delete") == 0) // Delete can only work on existing graph.
 			{
-				
+				if (command[1].compare("(") != 0 || command.size() != 4)
+				{
+					throw gcalc::GraphException("Invalid syntax");
+				}
+				deleteGraph(symbol_map, command[2]);
 			}
 			else // assignment
 			{
+				if (gcalc::GraphHelper::checkGraphName(command[0]))
+				{
+					if (command[1].compare("=") != 0)
+					{
+						throw gcalc::GraphException("Unrecognized command");
+					}
+					else
+					{
+						if (symbol_map.find(command[0]) != symbol_map.end())
+						{
+							throw gcalc::GraphException("Variable already exist");
+						}
+						else
+						{
+							// TODO: temp check. need to add a functionality for more than one graph
+							auto graph_start = command.begin() + 2;
+							std::vector<std::string> temp_vector(graph_start, command.end());
 
+							symbol_map.insert(std::pair<std::string, std::shared_ptr<gcalc::Graph>>(command[0], gcalc::GraphHelper::commandToGraph(temp_vector)));
+						}
+					}
+				}
 			}
 		}
 	}
