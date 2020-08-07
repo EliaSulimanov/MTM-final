2d245c1b95b6b1a28daf3b2b6376a99c2f4f12ee
Elia Sulimanov <eliasulimanov@gmail.com>
2020-08-07 19:29:18 +0300

    Done all the py module

--- Graph.h
+++ Graph.h
@@ -32,11 +32,9 @@ graph* addEdge(graph* graph, char* v1, char* v2);
 void disp(graph* graph);
 
 graph* graphUnion(graph* graph_in1, graph* graph_in2, graph* graph_out);
-/*
-
-graph graphIntersection(graph& graph_in1, graph& graph_in2, graph& graph_out);
-graph graphDifference(graph& graph_in1, graph& graph_in2, graph& graph_out);
-graph graphProduct(graph& graph_in1, graph& graph_in2, graph& graph_out);
-graph graphComplement(graph& graph_in, graph& graph_out);*/
+graph* graphIntersection(graph* graph_in1, graph* graph_in2, graph* graph_out);
+graph* graphDifference(graph* graph_in1, graph* graph_in2, graph* graph_out);
+graph* graphProduct(graph* graph_in1, graph* graph_in2, graph* graph_out);
+graph* graphComplement(graph* graph_in, graph* graph_out);
 #endif
 
